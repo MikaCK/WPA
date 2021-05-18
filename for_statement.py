0e85dc2d87becd20daad05e72d4b3552ac9421a9
ziczaces <ziczaces@hotmail.com>
2021-05-18 06:35:38 +0200

    for_statement

--- for_statement.py
+++ for_statement.py
@@ -1,18 +1,14 @@
-
-
 year = input("Insert start year: ")
-yearUntil = input("Insert end year: ")
+year_until = input("Insert end year: ")
 year = int(year)
-yearUntil = int(yearUntil)
+year_until = int(year_until)
 
-if year > yearUntil:
+if year > year_until:
     print("your starting year is greater than the end year")
 
-while year <= yearUntil:
+for year in range (year, year_until):
 
     if (year % 4 == 0 and year % 100 != 0) or (year % 4 == 0 and year % 400 == 0):
         print("Rok " + str(year) + " je prestupny")
-    else:
-        print("Rok " + str(year) + " je neprestupny")
 
-    year = year + 1
+
